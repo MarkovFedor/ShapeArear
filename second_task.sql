1ef570742765d40cd5d4cff9121eb19c1544ff5e
Markov_Fedor <64787875+MarkovFedor@users.noreply.github.com>
2022-06-28 20:44:38 +0500

    Add files via upload

---
+++ second_task.sql
@@ -0,0 +1,5 @@
+﻿select pr.Name, cat.Name from Product as pr
+                                  left join Product_to_Category as PtC
+                                            on pr.id = PtC.product_id
+                                  left join Category as cat
+                                            on cat.id = PtC.category_id
